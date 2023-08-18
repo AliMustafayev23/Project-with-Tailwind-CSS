cafc0582bf27f60176078425363776f561356697
AliMustafayev23 <ali.mustafayev.2002n@gmail.com>
2023-08-18 21:37:58 +0400

    container class and fonts added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        "regal-blue": "#243c5a",
+      },
+    },
   },
   plugins: [],
 };
-
