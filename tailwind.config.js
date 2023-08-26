26dd6eb2ef374318a1cbc091c3bc584f042caba3
AliMustafayev23 <ali.mustafayev.2002n@gmail.com>
2023-08-26 14:48:40 +0400

    header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         "2xl": "6rem",
       },
     },
+    fontSize: {
+      sm: ["14px", "20px"],
+      base: ["16px", "24px"],
+      lg: ["20px", "28px"],
+      xl: ["24px", "32px"],
+      "2xl": ["36px", "36px"],
+    },
     extend: {
       fontFamily: {
         gemunu: ["Gemunu Libre", "sans-serif"],
